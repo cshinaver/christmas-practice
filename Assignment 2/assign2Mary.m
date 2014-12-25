c516f4b497c8d3cd5ee56ccf686739f037f98ab1
Mary Connolly <maryconnolly@marys-mbp.home>
2014-12-25 13:53:45 -0500

    Mary's second assignment

---
+++ Assignment 2/assign2Mary.m
@@ -0,0 +1,22 @@
+#import <Foundation/Foundation.h>
+
+int main(int argv, const char *argc[])
+{
+	@autoreleasepool {
+
+		NSString *str1 = @"On April ", *str2 = @"th, I like to turn up with Fancy Feast. Meow bitchez hashtag "; 
+
+		int number = 20;
+
+		NSNumber *number2 = [NSNumber numberWithInteger: 420];
+
+		NSArray *array = [NSArray arrayWithObjects: str1, [NSNumber numberWithInteger: number], str2, number2, nil];
+
+		NSString *str = [array objectAtIndex: 0];
+
+		NSString *string = [str stringByAppendingFormat: @"%@%@%@\n", [array objectAtIndex: 1], [array objectAtIndex: 2], [array objectAtIndex: 3]];
+
+		printf("%s", [string UTF8String]);
+	}
+
+}

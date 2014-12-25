065f8e118647d6fac651b472567d02daa171ab01
Mary Connolly <maryconnolly@marys-mbp.home>
2014-12-24 22:01:32 -0500

    Mary's Hello World program

---
+++ Assignment 1/helloWorldMary.m
@@ -0,0 +1,13 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char *argv[])
+{
+	@autoreleasepool {
+
+		NSLog(@"Hello World.\nHello John.\nHello Charles.\nHello computer.\nThere is no one else to say hello to.\nNo one.\nFancy feast.\n");
+
+	return 0;
+
+	}
+
+}
